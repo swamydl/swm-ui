bd4a1fd1ca1a79489fccb974a508d7417923cffd
SwamyDL <thippeswamyswamy7483@gmail.com>
2025-07-29 17:50:51 +0530

    Connected pages and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,24 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
-
-import "./App.css";
-
-function AnimatedRoutes() {
-  const location = useLocation();
-
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </AnimatePresence>
-  );
-}
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-800 dark:text-white transition-colors">
+      <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
-          <AnimatedRoutes />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
         </main>
         <Footer />
       </div>
